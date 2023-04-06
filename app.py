11485638e06ac6c54eff00691d0669a5cd45d304
EJ <58092324+ej-b@users.noreply.github.com>
2023-04-06 10:23:03 +0100

    created html and css

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from flask import Flask, render_template
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templateFiles',
+            static_folder='staticFiles')
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
