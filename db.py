da95f96c29379b06f93a9022135d2eb50e51f23d
Rharm Manju <rharm@Rharms-MacBook-Air.local>
2023-04-06 11:41:53 +0100

    db stuff

--- db.py
+++ db.py
@@ -3,14 +3,22 @@ from faker import Faker
 import random
 
 # Create a connection to the database
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('example.db', check_same_thread=False)
 
 # Create a cursor object to execute SQL commands
 c = conn.cursor()
-
+c.execute("DROP table IF EXISTS volunteers;")
+c.execute("DROP table IF EXISTS school;")
+c.execute("DROP table IF EXISTS event;")
+c.execute("DROP table IF EXISTS eventschool;")
 # Create the volunteers table if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS volunteers
-             (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, points INTEGER, location TEXT)''')
+             (volunteerid INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, points INTEGER, location TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS school
+             (schid INTEGER PRIMARY KEY, location TEXT, city TEXT, name TEXT, info TEXT)''')
+c.execute('''CREATE TABLE IF NOT EXISTS event
+             (eventid INTEGER PRIMARY KEY, name TEXT, schid INT, info TEXT)''')
+
 
 # Create a Faker instance to generate random names
 fake = Faker()
@@ -19,10 +27,10 @@ fake = Faker()
 cities = ['London', 'Birmingham', 'Leeds', 'Sheffield', 'Bradford', 'Manchester', 'Liverpool', 'Newcastle upon Tyne', 'Nottingham', 'Bristol', 'Brighton', 'Cambridge', 'Canterbury', 'Chester', 'Coventry', 'Durham', 'Exeter', 'Gloucester', 'Hull', 'Lancaster', 'Leicester', 'Lincoln', 'Oxford', 'Peterborough', 'Plymouth', 'Portsmouth', 'Reading', 'St Albans', 'Salisbury', 'Southampton', 'Swansea', 'Wolverhampton', 'York']
 
 # Generate 1000 rows of dummy data
-for i in range(100):
+for i in range(20):
     firstname = fake.first_name()
     lastname = fake.last_name()
-    points = random.randint(0, 100)
+    points = random.randint(0, 250)
     location = random.choice(cities)
     c.execute(f"INSERT INTO volunteers (firstname, lastname, points, location) VALUES ('{firstname}', '{lastname}', {points}, '{location}')")
 
@@ -36,11 +44,6 @@ c.execute("SELECT * FROM volunteers")
 rows = c.fetchall()
 
 # Print the column names
-print("ID\tFIRSTNAME\tLASTNAME\tPOINTS\tLOCATION")
 
-# Print each row of data
-for row in rows:
-    print(f"{row[0]}\t{row[1]}\t\t{row[2]}\t\t{row[3]}\t{row[4]}")
 
 # Close the connection
-conn.close()
