3cf83e20c3e58a5df56b3dade583d005cdbc864b
Haoran-Jie <2291000484@qq.com>
2023-04-06 10:19:58 +0100

    added four pages for app

--- app.py
+++ app.py
@@ -3,8 +3,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+# Dash Board
 def hello():
     return 'Hello, World!'
+
+@app.route('/provider/dashboard')
+def provider_dashboard():
+    return "provider_dashboard"
+
+@app.route('/provider/submit')
+def provider_submit():
+    return "provider_submit"
+
+@app.route('/volunteer/apply')
+def volunteer_apply():
+    return "volunteer_apply"
+
+@app.route('volunteer/ranking')
+def volunteer_ranking():
+    return "volunteer_ranking"
 
 if __name__ == '__main__':
     app.run()
